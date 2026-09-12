b12c535577b969875b5e293a23eee14b59434b01
Aleksandr <aleksandrkrauskin190@gmail.com>
2026-09-13 00:02:17 +0300

    Исправлен vite.config

--- frontend_project/vite.config.js
+++ frontend_project/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/plugin-react' // ✏️ ИСПРАВЛЕНО (было @vitejs/react-plugin)
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    // 🆕 Это заставит Vite на домашнем ПК перенаправлять относительные пути /api/ на Django порт 8000
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000', 
+        target: 'http://backend:8000', 
         changeOrigin: true,
       },
       '/admin': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://backend:8000',
         changeOrigin: true,
       }
     }
